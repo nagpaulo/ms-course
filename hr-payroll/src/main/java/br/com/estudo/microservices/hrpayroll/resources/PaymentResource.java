66f5df0f8852afca40c26de3bd50d5a677291a44
Paulo Roberto Mesquita da Silva <nagpaulo@gmail.com>
2021-01-03 17:59:03 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/resources/PaymentResource.java
+++ hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/resources/PaymentResource.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.estudo.microservices.hrpayroll.entities.Payment;
 import br.com.estudo.microservices.hrpayroll.services.PaymentService;
 
@@ -18,9 +20,14 @@ public class PaymentResource {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public Payment getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		return this.service.getPayment(workerId, days)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pagamento não registrado!"));
+	}
+	
+	public Payment getPaymentAlternative(Long workerId, Integer days) {
+		return Payment.builder().name("Brann").dailyIncome(400.0).days(days).build();
 	}
 }
