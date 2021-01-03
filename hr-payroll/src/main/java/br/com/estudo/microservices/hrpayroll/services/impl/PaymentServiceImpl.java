5f6afed0ec34befd3dc4737413ab99287c53634c
Paulo Roberto Mesquita da Silva <nagpaulo@gmail.com>
2021-01-03 16:29:43 -0300

    Feign Client

--- hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/services/impl/PaymentServiceImpl.java
@@ -11,23 +11,18 @@ import org.springframework.web.client.RestTemplate;
 
 import br.com.estudo.microservices.hrpayroll.entities.Payment;
 import br.com.estudo.microservices.hrpayroll.entities.Worker;
+import br.com.estudo.microservices.hrpayroll.feignclients.WorkerFeignClient;
 import br.com.estudo.microservices.hrpayroll.services.PaymentService;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
 	
 	@Autowired
-	private RestTemplate restTemplate;
-	
-	@Value("${hr-worker.host}")
-	private String workerHost;
+	private WorkerFeignClient workerFeignClient;
 
 	@Override
-	public Optional<Payment> getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", String.valueOf(workerId));
-		
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+	public Optional<Payment> getPayment(long workerId, int days) {		
+		Worker worker = workerFeignClient.buscarWorkersPorId(workerId);
 		return Optional.of(Payment.builder().name(worker.getName()).dailyIncome(worker.getDailyIncome()).days(days).build());
 	}
 
