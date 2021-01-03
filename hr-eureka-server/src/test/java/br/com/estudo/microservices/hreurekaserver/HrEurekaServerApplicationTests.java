2f4944f9aba7720cdb6c330520af3516dfc3b71b
Paulo Roberto Mesquita da Silva <nagpaulo@gmail.com>
2021-01-03 17:01:44 -0300

    Eureka Server - Creater

---
+++ hr-eureka-server/src/test/java/br/com/estudo/microservices/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.microservices.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
