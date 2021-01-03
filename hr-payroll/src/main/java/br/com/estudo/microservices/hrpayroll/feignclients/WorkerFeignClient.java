5c7444321c6b3b3dde4dc01d7d0fe1bea8806625
Paulo Roberto Mesquita da Silva <nagpaulo@gmail.com>
2021-01-03 16:55:45 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/br/com/estudo/microservices/hrpayroll/feignclients/WorkerFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import br.com.estudo.microservices.hrpayroll.entities.Worker;
 
 @Component
-@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+@FeignClient(name = "hr-worker", path = "/workers")
 public interface WorkerFeignClient {
 	
 	@GetMapping("{id}")
--- hr-worker/src/main/java/br/com/estudo/microservices/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/estudo/microservices/hrworker/resources/WorkerResource.java
@@ -3,6 +3,7 @@ package br.com.estudo.microservices.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudo.microservices.hrworker.entities.Worker;
 import br.com.estudo.microservices.hrworker.services.WorkerService;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/workers")
+@Slf4j
 public class WorkerResource {
+	
+	@Autowired
+	private Environment env;
 	
 	@Autowired
 	private WorkerService service;
@@ -25,6 +31,7 @@ public class WorkerResource {
 	
 	@GetMapping("{id}")
 	public Worker buscarWorkersPorId(@PathVariable Long id) {
+		log.info("Port:"+env.getProperty("local.server.port"));		
 		return this.service.buscarWorkerPorId(id);
 	}
 }
