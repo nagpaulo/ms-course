f1234a17b72410692fd634d0c04ada852847b242
Paulo Roberto Mesquita da Silva <nagpaulo@gmail.com>
2021-01-03 21:34:57 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/br/com/estudo/microservices/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/estudo/microservices/hrworker/resources/WorkerResource.java
@@ -3,10 +3,13 @@ package br.com.estudo.microservices.hrworker.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudo.microservices.hrworker.entities.Worker;
@@ -17,6 +20,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/workers")
 @Slf4j
 public class WorkerResource {
+	
+	@Value("${test.config}")
+	private String testConfig;
 	
 	@Autowired
 	private Environment env;
@@ -33,5 +39,11 @@ public class WorkerResource {
 	public Worker buscarWorkersPorId(@PathVariable Long id) {
 		log.info("Port:"+env.getProperty("local.server.port"));		
 		return this.service.buscarWorkerPorId(id);
+	}
+	
+	@GetMapping("/configs")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void test() {
+		log.info("Config = " + testConfig);
 	}
 }
